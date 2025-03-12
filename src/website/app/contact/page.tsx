9f8e667d1bf8c85898c5f82575686b5a9780cb62
Jamey Stock <jameystock.dev@gmail.com>
2025-03-11 21:57:16 -0400

    fill out contact page

--- src/website/app/contact/page.tsx
+++ src/website/app/contact/page.tsx
@@ -5,5 +5,33 @@ export const metadata: Metadata = {
 };
 
 export default function ContactMe() {
-  return <h1>Contact Me</h1>;
+  return (
+    <>
+      <form className="mt-4">
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">
+            Email Address
+          </label>
+          <input name="email" type="email" className="form-control" required />
+        </div>
+
+        <div className="form-group mt-4">
+          <label className="form-label" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            name="message"
+            className="form-control"
+            rows={10}
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary mt-5">
+          <i className="bi bi-envelope-fill me-2" />
+          Send
+        </button>
+      </form>
+    </>
+  );
 }
